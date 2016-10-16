1f2cde3f0077bdfb238a39a064994af79f3623bf
Brady Rhora <brady0423@gmail.com>
2016-10-16 14:53:09 -0400

    Update private.cs

--- LennyBot/private.cs
+++ LennyBot/private.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LennyBot
-{
-    class @private
-    {
-        public static string token = "MjMwNTA4MTgwNzkzMzI3NjE2.CuF4tg.UqSDHzELu2xyFgGKS8gOfrxDadY";
-    }
-}
+( ͡° ͜ʖ ͡°)
